9f49b95ee6253ea4306caf1337fa0f4ccf0d40e0
Richard Parsons <parsons.rk@gmail.com>
2020-04-16 09:22:11 +0200

    Refactor field extension component

---
+++ src/FieldExtension.tsx
@@ -0,0 +1,114 @@
+import {
+    Button,
+    Note,
+    TextField,
+    TextInput,
+    ValidationMessage
+} from '@contentful/forma-36-react-components';
+import React, { useCallback, useEffect, useState } from 'react';
+
+import { FieldExtensionProps } from './typings';
+import axios from 'axios';
+
+type Metadata = {
+    trackId?: string;
+    streamUrl?: string;
+    samples?: number[];
+};
+
+type InstallationParameters = {
+    clientId: string;
+};
+
+type SoundCloudTrack = {
+    waveform_url: string;
+    stream_url: string;
+};
+
+type SoundCloudWaveform = {
+    samples: number[];
+};
+
+const FieldExtension = ({ sdk }: FieldExtensionProps) => {
+    const { clientId } = sdk.parameters.installation as InstallationParameters;
+    const savedValue = sdk.field.getValue() as Metadata;
+    const [trackId, setTrackId] = useState(savedValue.trackId);
+    const [streamUrl, setStreamUrl] = useState(savedValue.streamUrl);
+    const [samples, setSamples] = useState(savedValue.samples);
+    const [error, setError] = useState<Error>();
+
+    useEffect(() => {
+        sdk.window.startAutoResizer();
+    }, [sdk.window]);
+
+    useEffect(() => {
+        sdk.field.setValue({
+            trackId,
+            streamUrl,
+            samples
+        });
+    }, [trackId, streamUrl, samples, sdk.field]);
+
+    const updateTrackId = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+        setStreamUrl(undefined);
+        setSamples(undefined);
+        setTrackId(event.target.value);
+    }, []);
+
+    const fetchMetadata = useCallback(() => {
+        axios
+            .get<SoundCloudTrack>(
+                `https://api.soundcloud.com/tracks/${trackId}?client_id=${clientId}`
+            )
+            .then(({ data }) => {
+                setStreamUrl(data.stream_url);
+                const samplesUrl = data.waveform_url.replace('.png', '.json');
+                return axios.get<SoundCloudWaveform>(samplesUrl);
+            })
+            .then(({ data }) => {
+                const maxValue = Math.max(...data.samples);
+                const samples = data.samples.map((x: number) => x / maxValue);
+                setSamples(samples);
+            })
+            .catch((error: Error) => {
+                setError(error);
+            });
+    }, [clientId, trackId]);
+
+    const handleClick = useCallback(() => {
+        setError(undefined);
+        setSamples(undefined);
+        fetchMetadata();
+    }, [setError, setSamples, fetchMetadata]);
+
+    return (
+        <>
+            <section>
+                <TextField
+                    id="trackId"
+                    name="streamUrl"
+                    labelText="Stream URL"
+                    required
+                    validationMessage="Enter valid track ID."
+                    textInputProps={{ value: trackId, type: 'number', onChange: updateTrackId }}
+                />
+                {error && <ValidationMessage>Invalid track id</ValidationMessage>}
+
+                <Button onClick={handleClick} disabled={!trackId} icon="Settings">
+                    Generate Metadata
+                </Button>
+
+                <TextField
+                    id="streamUrl"
+                    name="streamUrl"
+                    labelText="Stream URL"
+                    required
+                    validationMessage="Click 'Generate Metadata'"
+                    textInputProps={{ value: streamUrl }}
+                />
+            </section>
+        </>
+    );
+};
+
+export default FieldExtension;
--- src/index.tsx
+++ src/index.tsx
@@ -2,114 +2,11 @@ import '@contentful/forma-36-react-components/dist/styles.css';
 import './index.css';
 
 import { AppExtensionSDK, FieldExtensionSDK, init, locations } from 'contentful-ui-extensions-sdk';
-import {
-  Button,
-  Icon,
-  Note,
-  TextInput,
-  ValidationMessage
-} from '@contentful/forma-36-react-components';
-import React, { useCallback, useEffect, useState } from 'react';
 
 import AppConfig from './AppConfig';
-import { FieldExtensionProps } from './typings';
-import axios from 'axios';
+import FieldExtension from './FieldExtension';
+import React from 'react';
 import { render } from 'react-dom';
-
-type Metadata = {
-  trackId?: string;
-  streamUrl?: string;
-  samples?: number[];
-};
-
-type InstallationParameters = {
-  clientId: string;
-};
-
-type SoundCloudTrack = {
-  waveform_url: string;
-  stream_url: string;
-};
-
-type SoundCloudWaveform = {
-  samples: number[];
-};
-
-export const FieldExtension = ({ sdk }: FieldExtensionProps) => {
-  const { clientId } = sdk.parameters.installation as InstallationParameters;
-  const savedValue = sdk.field.getValue() as Metadata;
-  const [trackId, setTrackId] = useState(savedValue.trackId);
-  const [streamUrl, setStreamUrl] = useState(savedValue.streamUrl);
-  const [samples, setSamples] = useState(savedValue.samples);
-  const [error, setError] = useState<Error>();
-
-  useEffect(() => {
-    sdk.window.startAutoResizer();
-  }, [sdk.window]);
-
-  useEffect(() => {
-    sdk.field.setValue({
-      trackId,
-      streamUrl,
-      samples
-    });
-  }, [trackId, streamUrl, samples, sdk.field]);
-
-  const updateTrackId = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => setTrackId(event.target.value),
-    []
-  );
-
-  const fetchMetadata = useCallback(() => {
-    axios
-      .get<SoundCloudTrack>(`https://api.soundcloud.com/tracks/${trackId}?client_id=${clientId}`)
-      .then(({ data }) => {
-        setStreamUrl(data.stream_url);
-        const samplesUrl = data.waveform_url.replace('.png', '.json');
-        return axios.get<SoundCloudWaveform>(samplesUrl);
-      })
-      .then(({ data }) => {
-        const maxValue = Math.max(...data.samples);
-        const samples = data.samples.map((x: number) => x / maxValue);
-        setSamples(samples);
-      })
-      .catch((error: Error) => {
-        setError(error);
-      });
-  }, [clientId, trackId]);
-
-  const handleClick = useCallback(() => {
-    setError(undefined);
-    setSamples(undefined);
-    fetchMetadata();
-  }, [setError, setSamples, fetchMetadata]);
-
-  return (
-    <>
-      <section>
-        <TextInput
-          name="trackId"
-          type="number"
-          value={trackId}
-          className="f36-margin-bottom--m"
-          onChange={updateTrackId}
-          width="small"
-        />
-
-        <Button onClick={handleClick} disabled={!trackId}>
-          Fetch Metadata
-        </Button>
-        {samples && (
-          <>
-            <Icon color="primary" size="large" icon="Edit" /> Audio waveform retrieved with{' '}
-            {samples.length} samples
-          </>
-        )}
-      </section>
-      {error && <ValidationMessage>Invalid track id</ValidationMessage>}
-    </>
-  );
-};
 
 init(sdk => {
   if (sdk.location.is(locations.LOCATION_APP_CONFIG)) {
